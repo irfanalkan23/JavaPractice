2e570c798049a40b6e4a14b1d0f95a83eebf1aec
irfan <23pointalpha@gmail.com>
2022-10-17 13:01:26 +0300

    initial commit

--- src/practice/patika/Employee.java
+++ src/practice/patika/Employee.java
@@ -1,20 +1,62 @@
 package practice.patika;
 
+import java.util.InputMismatchException;
+
 public class Employee {
 
-    public String name;
-    public double salary;
-    public double workHours;
-    public int hireYear;
+    private String name;
+    private double salary;
+    private double workHours;
+    private int hireYear;
 
     public Employee(String name, double salary, double workHours, int hireYear) {
+        setName(name);
+        setSalary(salary);
+        setWorkHours(workHours);
+        setHireYear(hireYear);
+    }
+
+    public void setName(String name) {
+        if ( name.isEmpty() || !name.matches("[a-zA-Z]++(?: ++[a-zA-Z]++)*+")){
+            throw new InputMismatchException("Invalid name: " + name);
+        }
         this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        if(salary <= 0){
+            throw new InputMismatchException("Salary can not be zero or negative: " + salary);
+        }
         this.salary = salary;
+    }
+
+    public void setWorkHours(double workHours) {
+        if (workHours < 0){
+            throw new InputMismatchException("Work hours can not be negative: " + workHours);
+        }
         this.workHours = workHours;
+    }
+
+    /**
+     * Hire year can not be later than 2021 (current date), or before 1921 (100 working years).
+     * @param hireYear
+     */
+    public void setHireYear(int hireYear) {
+        if (hireYear > 2021 || hireYear < 1921){
+            throw new InputMismatchException("Hire year can not be more than 2021 " +
+                    "or less than 1921: " + hireYear);
+        }
         this.hireYear = hireYear;
     }
 
-    public double tax(double salary){
+    /**
+     * Calculates the tax to salary
+     * No tax if salary is less than 1000TL
+     * 3% tax if salary is more than or equal to 1000TL
+     * @param salary as double
+     * @return tax amount as double
+     */
+    private double tax(double salary){
         double taxAmount;
         if (salary<1000){
             taxAmount = 0.0;
@@ -24,7 +66,13 @@ public class Employee {
         return taxAmount;
     }
 
-    public double bonus(double workHours){
+    /**
+     * Calculates the bonus payment.
+     * 30TL bonus per hour over 40 working hours per week.
+     * @param workHours
+     * @return bonus amount
+     */
+    private double bonus(double workHours){
         if (workHours>40){
             return (workHours-40)*30;
         } else {
@@ -32,7 +80,16 @@ public class Employee {
         }
     }
 
-    public double raiseSalary(int hireYear){
+    /**
+     * Calculates raise in salary based on hire year.
+     * Tax and bonus are taken into account!
+     * 5% increase in salary if less than 10 years
+     * 10% increase in salary if in between 9 to 20 years
+     * 15% increase in salary if more than 19 years
+     * @param hireYear
+     * @return salary raise amount
+     */
+    private double raiseSalary(int hireYear){
         double raiseSalaryAmount;
         double salaryWithTaxAndBonus = salary + tax(salary) + bonus(workHours);
         if ( (2021-hireYear) < 10 ) {
@@ -43,7 +100,6 @@ public class Employee {
             raiseSalaryAmount = salaryWithTaxAndBonus * 0.15;
         }
         return raiseSalaryAmount;
-
     }
 
 
@@ -51,7 +107,7 @@ public class Employee {
     public String toString() {
         return "Employee{" +
                 "name='" + name + '\'' +
-                ", salary=" + ( salary + raiseSalary(hireYear) ) +
+                ", salary=" + salary +
                 ", workHours=" + workHours +
                 ", hireYear=" + hireYear +
                 ", tax =" + tax(salary) +
--- src/practice/patika/EmployeeTest.java
+++ src/practice/patika/EmployeeTest.java
@@ -2,7 +2,7 @@ package practice.patika;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Ali",5500.0, 50.0,2011);
+        Employee employee1 = new Employee("Ali Can Berk",2000.0, 50.0,2011);
         System.out.println(employee1);
     }
 }
